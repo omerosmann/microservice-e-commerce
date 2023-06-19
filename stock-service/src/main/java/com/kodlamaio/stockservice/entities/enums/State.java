5e488e92974311fc05a40bf6a67ad4a00a86c2df
Ömer Osman <113211889+omerosmann@users.noreply.github.com>
2023-06-19 11:14:15 +0300

    Delete stock-service/src/main/java/com/kodlamaio/stockservice directory

--- stock-service/src/main/java/com/kodlamaio/stockservice/entities/enums/State.java
+++
@@ -1,6 +0,0 @@
-package com.kodlamaio.stockservice.entities.enums;
-
-public enum State {
-    Active,
-    Passive
-}
--- stock-service/src/main/java/com/kodlamaio/stockservice/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kodlamaio.stockservice.repository;
-
-import com.kodlamaio.stockservice.entities.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface CategoryRepository extends JpaRepository<Category, UUID> {}
